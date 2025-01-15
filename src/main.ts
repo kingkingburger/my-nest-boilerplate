3cac5a1448bf98e5c6e6ac4b4103dbc4351c7935
minho <dnjsalsgh123@gmail.com>
2025-01-15 17:19:52 +0900

    update: 에러코드 반환을 위헤 strip-ansi 패키지 설치

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { TransformResponseInterceptor } from './config/interceptor/transform-response.interceptor';
+import { HttpExceptionFilter } from './config/filter/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // 기본 CORS 활성화
   app.enableCors();
+
+  // 전역 Interceptor 등록
+  app.useGlobalInterceptors(new TransformResponseInterceptor());
+
+  // 전역 Filter 등록
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   // swagger 적용
   const config = new DocumentBuilder()
